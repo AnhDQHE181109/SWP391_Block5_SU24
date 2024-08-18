39987bad22b0dcc2cb1edaa93aac37be6d527c68
AnhDQHE181109 <lap1234.lpo@gmail.com>
2024-08-18 07:44:23 +0700

    Implementing product details
    Displaying a product's details

--- SWP391_Block5_SU24/src/java/entity/ProductDetails.java
+++ SWP391_Block5_SU24/src/java/entity/ProductDetails.java
@@ -9,5 +9,99 @@ package entity;
  * @author ASUS
  */
 public class ProductDetails {
+    private int productID;
+    private String productName;
+    private String origin;
+    private String material;
+    private double price;
+    private int totalQuantity;
+    private String categoryName;
+    private String brandName;
+    private int productStatus;
+
+    public ProductDetails(int productID, String productName, String origin, String material, double price, int totalQuantity, String categoryName, String brandName, int productStatus) {
+        this.productID = productID;
+        this.productName = productName;
+        this.origin = origin;
+        this.material = material;
+        this.price = price;
+        this.totalQuantity = totalQuantity;
+        this.categoryName = categoryName;
+        this.brandName = brandName;
+        this.productStatus = productStatus;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getTotalQuantity() {
+        return totalQuantity;
+    }
+
+    public void setTotalQuantity(int totalQuantity) {
+        this.totalQuantity = totalQuantity;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public int getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(int productStatus) {
+        this.productStatus = productStatus;
+    }
+    
     
 }
---
+++ SWP391_Block5_SU24/src/java/entity/ProductStockDetails.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ProductStockDetails {
+    private int productID;
+    private int size;
+    private String color;
+    private int stockQuantity;
+    private String imageURL;
+
+    public ProductStockDetails(int productID, int size, String color, int stockQuantity, String imageURL) {
+        this.productID = productID;
+        this.size = size;
+        this.color = color;
+        this.stockQuantity = stockQuantity;
+        this.imageURL = imageURL;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+    
+    
+}
