a7e8f2631680954f6a34ca92c7bafcaf41943ae4
AnhDQHE181109 <lap1234.lpo@gmail.com>
2024-08-25 04:12:34 +0700

    Fixing displaying sold out products

--- SWP391_Block5_SU24/src/java/Controller/ProductDetailsController.java
+++ SWP391_Block5_SU24/src/java/Controller/ProductDetailsController.java
@@ -110,13 +110,13 @@ public class ProductDetailsController extends HttpServlet {
             }
             List<ProductStockDetails> productSizes = pdDAO.getSizesByColorAndProductID(productID, selectedColor);
             
-            int count = 0;
-            for (ProductStockDetails productSize : productSizes) {
-                if (productSize.getStockQuantity() == 0) {
-                    productSizes.remove(count);
-                }
-                count++;
-            }
+//            int count = 0;
+//            for (ProductStockDetails productSize : productSizes) {
+//                if (productSize.getStockQuantity() == 0) {
+//                    productSizes.remove(count);
+//                }
+//                count++;
+//            }
 
             
             if (selectedSize == null) {
